d6ae872186a7200922b9f0b5c3c123f346d5094a
raycham <stephenvanvo@gmail.com>
2025-11-25 19:41:20 +0800

    增加深度学习目录框架

--- 4_SGU/1_ML/2_regularization.py
+++ 4_SGU/1_ML/2_regularization.py
@@ -21,67 +21,58 @@ print(X.shape)
 print(y.shape)
 
 # 画出散点图，三个子图
-fig, ax = plt.subplots(1,3,figsize=(15,4))
-ax[0].scatter(X,y,c='r')
-ax[1].scatter(X,y,c='y')
-ax[2].scatter(X,y,c='b')
+fig, ax = plt.subplots(2,3,figsize=(15,8))
+ax[0,0].scatter(X,y,c='g')
+ax[0,1].scatter(X,y,c='y')
+ax[0,2].scatter(X,y,c='b')
 # plt.show()
 
 # 2. 划分训练集和测试集
 train_X,test_X,train_y,test_y = train_test_split(X,y,test_size=0.2,random_state=42)
 
+poly20 = PolynomialFeatures(degree=20)
+x_train = poly20.fit_transform(train_X)
+x_test = poly20.fit_transform(test_X)
+
+# 一、不加正则化
 # 3. 定义模型
 model = LinearRegression()
-
-# 一、欠拟合（直线）
-x_train1 = train_X
-x_test1 = test_X
-
 # 4. 训练模型
-model.fit(x_train1,train_y)
-
-# 打印查看模型参数
-print(model.coef_)
-print(model.intercept_)
-
+model.fit(x_train,train_y)
 # 5. 预测结果，计算误差
-y_pred1 = model.predict(x_test1)
-test_loss1 = mean_squared_error(test_y,y_pred1)
-train_loss1 = mean_squared_error(train_y,model.predict(x_train1))
+y_pred = model.predict(x_test)
+test_loss = mean_squared_error(test_y,y_pred)
 # 画出拟合曲线，并写出训练误差和测试误差
-ax[0].plot(X,model.predict(X),'r')
-ax[0].text(-3,1,f"测试误差：{test_loss1:.4f}")
-ax[0].text(-3,1.3,f"训练误差：{train_loss1:.4f}")
+ax[0,0].plot(X,model.predict(poly20.fit_transform(X)),'r')
+ax[0,0].text(-3,1,f"测试误差：{test_loss:.4f}")
+# 画所有系数的直方图
+ax[1,0].bar(np.arange(21),model.coef_.reshape(-1))
 
-# 二、恰好拟合（5次多项式）
-poly5 = PolynomialFeatures(degree=5)
-x_train2 = poly5.fit_transform(train_X)
-x_test2 = poly5.fit_transform(test_X)
+# 二 增加L1正则化项，Lasso回归
+lasso = Lasso(alpha=0.01)
 # 4. 训练模型
-model.fit(x_train2,train_y)
+lasso.fit(x_train,train_y)
 # 5. 预测结果，计算误差
-y_pred2 = model.predict(x_test2)
-test_loss2 = mean_squared_error(test_y,y_pred2)
-train_loss2 = mean_squared_error(train_y,model.predict(x_train2))
+y_pred = lasso.predict(x_test)
+test_loss = mean_squared_error(test_y,y_pred)
 # 画出拟合曲线，并写出训练误差和测试误差
-ax[1].plot(X,model.predict(poly5.fit_transform(X)),'r')
-ax[1].text(-3,1,f"测试误差：{test_loss2:.4f}")
-ax[1].text(-3,1.3,f"训练误差：{train_loss2:.4f}")
+ax[0,1].plot(X,lasso.predict(poly20.fit_transform(X)),'r')
+ax[0,1].text(-3,1,f"测试误差：{test_loss:.4f}")
+# 画所有系数的直方图
+ax[1,1].bar(np.arange(21),lasso.coef_.reshape(-1))
 
-# 三、过拟合(20次多项式)
-poly20 = PolynomialFeatures(degree=20)
-x_train3 = poly20.fit_transform(train_X)
-x_test3 = poly20.fit_transform(test_X)
+# 三 增加L2正则化项，岭回归
+ridge = Ridge(alpha=1)
 # 4. 训练模型
-model.fit(x_train3,train_y)
+ridge.fit(x_train,train_y)
 # 5. 预测结果，计算误差
-y_pred3 = model.predict(x_test3)
-test_loss3 = mean_squared_error(test_y,y_pred3)
-train_loss3 = mean_squared_error(train_y,model.predict(x_train3))
+y_pred = ridge.predict(x_test)
+test_loss = mean_squared_error(test_y,y_pred)
 # 画出拟合曲线，并写出训练误差和测试误差
-ax[2].plot(X,model.predict(poly20.fit_transform(X)),'r')
-ax[2].text(-3,1,f"测试误差：{test_loss3:.4f}")
-ax[2].text(-3,1.3,f"训练误差：{train_loss3:.4f}")
+ax[0,2].plot(X,ridge.predict(poly20.fit_transform(X)),'r')
+ax[0,2].text(-3,1,f"测试误差：{test_loss:.4f}")
+# 画所有系数的直方图
+ax[1,2].bar(np.arange(21),ridge.coef_.reshape(-1))
 
 plt.show()
 
