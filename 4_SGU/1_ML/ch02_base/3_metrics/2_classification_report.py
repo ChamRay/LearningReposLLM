b0d00731d45e637837ffcb28b7f40f9b34a760ed
raycham <stephenvanvo@gmail.com>
2025-11-26 21:40:19 +0800

    完成分类性能指标

---
+++ 4_SGU/1_ML/ch02_base/3_metrics/2_classification_report.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import make_classification # 用于生成分类数据集
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression # 逻辑回归分类模型
+from sklearn.metrics import classification_report # 生成分类评估报告
+
+
+# 1. 生成数据
+X,y = make_classification(n_samples=1000,n_features=20,n_classes=2,random_state=42)
+print(X.shape)
+print(y.shape)
+
+# 2. 划分数据集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 3. 定义一个分类模型
+modal = LogisticRegression()
+
+# 4. 模型训练
+modal.fit(X_train, y_train)
+# 5. 预测
+y_pred = modal.predict(X_test)
+# 6. 生成分类评估报告
+print(classification_report(y_test, y_pred))
+
+# 获取预测正类的概率值
+y_pred_proba = modal.predict_proba(X_test)[:,1]
+
+from sklearn.metrics import roc_auc_score
+roc_auc = roc_auc_score(y_test, y_pred_proba)
+print(roc_auc)
+
